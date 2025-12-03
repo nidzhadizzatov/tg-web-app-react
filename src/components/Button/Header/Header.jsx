c8f357fe4a3eb7f645bb910f346d4b5f73455e4a
Ниджад Иззатов <izzatov.nidzhad@mail.ru>
2025-12-03 17:35:17 +0300

    s

--- src/components/Button/Header/Header.jsx
+++ src/components/Button/Header/Header.jsx
@@ -4,11 +4,12 @@ import React from 'react';
 import { useTelegram } from '../../../hooks/useTelegram.js';
 
 function Header() {
-  const { onToggleButton, onClose } = useTelegram();
-
+  const { onToggleButton, onClose,user } = useTelegram();
+const firstName = user?.first_name || 'Guest';
   return (
     <header className="App-header">
       <button onClick={onToggleButton}>Toggle MainButton</button>
+        <span>Hello, {firstName}</span>
       <button onClick={onClose}>Закрыть</button>
     </header>
   );
